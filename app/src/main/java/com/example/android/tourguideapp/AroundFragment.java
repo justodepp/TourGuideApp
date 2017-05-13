68c2c6c715d0398852d3c8da76df4438b3429863
Gianni Cavalli <justodepp@users.noreply.github.com>
2017-05-13 20:02:37 +0200

    Update AroundFragment.java

--- app/src/main/java/com/example/android/tourguideapp/AroundFragment.java
+++ app/src/main/java/com/example/android/tourguideapp/AroundFragment.java
@@ -35,18 +35,6 @@ public class AroundFragment extends Fragment {
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
-        // Create an {@link PlaceAdapter}, whose data source is a list of {@link Word}s. The
-        // adapter knows how to create list items for each item in the list.
-        //PlaceAdapter adapter = new PlaceAdapter(getActivity(), places);
-
-        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
-        // There should be a {@link ListView} with the view ID called list, which is declared in the
-        // item_list.xml layout file.
-        //ListView listView = (ListView) rootView.findViewById(R.id.list);
-
-        // Make the {@link ListView} use the {@link PlaceAdapter} we created above, so that the
-        // {@link ListView} will display list items for each {@link Place} in the list.
-        //listView.setAdapter(adapter);
         return rootView;
     }
 
